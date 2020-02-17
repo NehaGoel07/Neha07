622986bc0dc6cbe6cc03a1a4e963de029cb5d1c9
NehaGoel07 <neha.goel@tothenew.com>
2020-02-17 17:34:05 +0530

    Intro

---
+++ Intro.Net/Q7(a)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Q7_a_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("a:"+a);
+        }
+    }
+}
---
+++ Intro.Net/Q7(b)/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello TTN")
+        Dim a As Integer = 5
+
+    End Sub
+End Module
