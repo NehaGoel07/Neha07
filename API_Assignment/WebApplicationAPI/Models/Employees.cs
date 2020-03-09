8077436c5f447fc59253b6262f6cb272d6f0e1cf
NehaGoel07 <neha.goel@tothenew.com>
2020-03-09 15:20:51 +0530

    API

---
+++ API_Assignment/WebApplicationAPI/Models/Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationAPI.Models
+{
+    public class Employees
+    {
+        //properties for the fields of table in database to retrieve and set values 
+        public string Emp_name { get; set; }
+
+        public string Emp_dob { get; set; }
+
+        public string Emp_salary { get; set; }
+
+        public string Manager_id { get; set; }
+
+        public bool IsTrainer { get; set; }
+    }
+}
