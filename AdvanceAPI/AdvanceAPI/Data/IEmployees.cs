c20d308fcebee62e129a5028b081f2724ed14cc8
NehaGoel07 <neha.goel@tothenew.com>
2020-03-11 16:06:31 +0530

    Push advance

---
+++ AdvanceAPI/AdvanceAPI/Data/IEmployees.cs
@@ -0,0 +1,20 @@
+﻿using AdvanceAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceAPI.Data
+{
+    //interface to implement dependency injection
+    public interface IEmployees
+    {
+        DataSet Disp();
+        bool InsertEmp(EmployeesCRUD emp1);
+        DataSet SingleData(int id);
+        bool UpdateData(EmployeesCRUD empData);
+        bool DeleteData(EmployeesCRUD empDel);
+
+    }
+}
