603612906b2feb2faafccd0fdbfbfcd42da291b7
NehaGoel07 <neha.goel@tothenew.com>
2020-04-10 21:13:30 +0530

    MVC Basic

---
+++ MVCBasics/Controllers/QuesOneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class QuesOneController : Controller
+    {
+        // GET: QuesOne
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCBasics/Models/Repository/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCBasics.Models.Repository
+{
+    interface IEmployee
+    {
+        List<TBL_Employees> Data();
+
+        bool EditData(TBL_Employees tBL_Employees);
+    }
+}
