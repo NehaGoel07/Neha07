5b3e52bb5d23645b600cbe975373a9bc78e4584f
NehaGoel07 <neha.goel@tothenew.com>
2020-07-10 11:55:48 +0530

    SideMenuDynamic

---
+++ SideFixedMenu/SideFixedMenu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SideFixedMenu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
